b3c2fb8f1b0a2fa1a68184984d8be2231afc6021
Anika Abreu <anika.abreu9@gmail.com>
2017-05-04 20:16:03 -0400

    all files

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Simple customer interface made with Angular 1.6/Rails.
 
+Styling by me.
+
 Credit for the tutorial:
 https://www.nopio.com/blog/angularjs-single-page-app/
-
-
--- app/assets/javascripts/angular/controllers/MainController.js
+++ app/assets/javascripts/angular/controllers/MainController.js
@@ -1,5 +1,6 @@
 var app = angular.module('app');
 
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.test = "Welcome to a simple user interface application";
+  $scope.test = "Welcome";
+  $scope.lines = "A simple user interface built with Angular 1.6 and Ruby on Rails"
 }]);
